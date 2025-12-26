b33b1a4590d77fe892e9a3e00b8abf61afae4227
蔡超 <chao.cai1@gmail.com>
2025-12-26 12:19:46 +0800

    配置云端Neo4j数据库

--- xjygraph.py
+++ xjygraph.py
@@ -23,7 +23,7 @@ TARGET_LABEL = "Danmu_xujiying"
 ADMIN_PASSWORD = "admin888"
 
 # 3. 数据库配置
-NEO4J_URI = "bolt://localhost:7687"
+NEO4J_URI = "neo4j+s://7eb127cc.databases.neo4j.io"
 NEO4J_USER = "neo4j"
 NEO4J_PASSWORD = "wE7pV36hqNSo43mpbjTlfzE7n99NWcYABDFqUGvgSrk"
 
